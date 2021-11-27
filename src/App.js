a0ef84628bbd5f3921284922817e3247d118adaf
AntonTymchenko <timchenkov_@ukr.net>
2021-11-27 21:31:30 +0200

    chuck

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "react-loader-spinner";
 import Header from "./components/Header";
 
 import { fetchChuck, fetchChuckCategories } from "./redux/chuck-operations";
@@ -8,6 +9,8 @@ import ChuckSays from "./components/ChuckSays";
 import "./App.css";
 
 function App() {
+  const categories = useSelector((state) => state.chuck.categories);
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchChuck());
@@ -16,8 +19,22 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Hero />
-      <ChuckSays />
+
+      {categories.length === 0 ? (
+        <Loader
+          className="loader"
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000}
+        />
+      ) : (
+        <>
+          <Hero />
+          <ChuckSays />
+        </>
+      )}
     </div>
   );
 }
